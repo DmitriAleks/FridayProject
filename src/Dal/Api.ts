117a2cde49b491f3a47d1faa3081fa1087b00b99
DmitriAleks <Dima.aleks.94@mail.ru>
2021-09-21 22:15:07 +0300

    created api for register

--- src/Dal/Api.ts
+++ src/Dal/Api.ts
@@ -6,10 +6,14 @@ const instance = axios.create({
     baseURL:  "http://localhost:7542/2.0/",
     withCredentials: true,
 })
-
 export const api = {
  inLogin(email: string, password: string, rememberMe: boolean) {
      const promise = instance.post("auth/login",{email,password, rememberMe})
      return promise
- }
+ },
+    inRegistr(email: string, password: string) {
+        const promise = instance.post("auth/register",{email,password, })
+        return promise
+    }
 }
+//register
